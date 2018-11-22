ff1fc40754b18b8aff374dcecbe8466a12e2665a
ngirault <45272558+ngirault@users.noreply.github.com>
2018-11-22 12:29:24 -0800

    Add files via upload

---
+++ DisplayCurrentWeather.java
@@ -0,0 +1,66 @@
+package javaDisplayWeather;
+
+import java.io.IOException;
+import java.util.Date;
+import org.bson.Document;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+/** 
+ *  DisplayCurrentWeather class displays weather information from mongoDB data
+ */
+public class DisplayCurrentWeather {
+	
+	static final String MONGODB_URI = "mongodb+srv://Admin:nimda8102@cluster0-cbecc.mongodb.net/admin";
+
+	public static void main(String[] args) {
+
+	  try {
+		  // Connect MongoDb
+		  MongoClientURI clientURI = new MongoClientURI(MONGODB_URI);
+		  MongoClient mongoClient = new MongoClient(clientURI);
+
+		  MongoDatabase mongoDatabase = mongoClient.getDatabase("MyMongoDB");
+		  MongoCollection < Document > collection = mongoDatabase.getCollection("weather");
+
+		  MongoCursor < Document > cursor = collection.find().iterator();
+		  System.out.println("Current Weather (" + new Date() + ") is :");
+		  try {
+			  while (cursor.hasNext()) {
+				  // Get data from Json & display
+				  displayCleanData(cursor.next().toJson());
+			  }
+		   } finally {
+		    cursor.close();
+		   }
+
+		  // Close MongoDb connection	
+		  mongoClient.close();
+		  
+	  	} catch (MongoException e) {
+		e.printStackTrace();
+	  	} catch (Exception e) {
+	    System.out.println(e);
+	  }
+
+	 }
+
+	 private static void displayCleanData(String json) throws JsonParseException, JsonMappingException, IOException {
+	  //create ObjectMapper instance
+	  ObjectMapper objectMapper = new ObjectMapper();
+	  
+	  //convert json string to object
+	  Location loc = objectMapper.readValue(json, Location.class);
+	  
+	  // display
+	  System.out.println("\n" + loc);
+	 }
+	}
---
+++ GetWeather.java
@@ -0,0 +1,120 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.bson.Document;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+/** 
+ *  GetWeather class get weather info from openweathermap and save them in mongoDB data
+ */
+public class GetWeather {
+	
+	static final String PROPERTY_FILE_LOC = "src/main/resources/config.properties";
+	static String appId = null;
+	static int limitRequest = 60;
+	static String[] cities;
+	
+	static MongoClient mongoClient;
+	static MongoCollection<Document> collection;
+	static final String MONGO_URI = "mongodb+srv://Admin:nimda8102@cluster0-cbecc.mongodb.net/admin";
+
+	static final String BASE_URL_OPENWEATHER = "http://api.openweathermap.org/data/2.5/weather?q=";
+			
+	public static void main(String[] args) {
+		// Get Properties File info
+		getPropertiesInfo();
+							        
+		RateLimiter limiter = RateLimiter.create(1.0/(60/limitRequest));	        
+		// Call openweathermap
+        while(true){
+		getDataFromOpenWeatherMap(limiter);
+		}		
+	}
+
+	private static void getPropertiesInfo() {						
+		try (InputStream in = new FileInputStream(PROPERTY_FILE_LOC)) {
+			Properties prop = new Properties();				
+			prop.load(in);
+
+			cities=prop.getProperty("cities").split(",");				
+			appId=prop.getProperty("appId");				
+			limitRequest=Integer.parseInt(prop.getProperty("limitrequest"));
+			
+		} catch (IOException e) {					           
+		            e.printStackTrace();
+		}			
+	}
+	
+	private static void getDataFromOpenWeatherMap(RateLimiter limiter) {			
+		try {
+			// Open MongoDB Connection
+			openMongoDB();
+			
+			// Call API
+			for (String c: cities) {						
+				URL url = new URL(BASE_URL_OPENWEATHER+c+"&APPID="+appId+"&units=imperial");					
+				limiter.acquire();				
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+	
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "+ conn.getResponseCode());
+				}
+		
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+				String resultJson = org.apache.commons.io.IOUtils.toString(br);				
+				conn.disconnect();
+				
+				//Save to MongoDB
+				saveToMongoDB(c,resultJson);												
+			}
+			
+			// Close MongoDB Connection
+			closeMongoDB();	
+			
+		} catch (MalformedURLException e) {
+		e.printStackTrace();
+		} catch (IOException e) {
+		e.printStackTrace();
+		} catch (MongoException e) {
+		e.printStackTrace();
+	    }
+				
+	}
+
+	private static void openMongoDB() {
+		MongoClientURI clientURI = new MongoClientURI(MONGO_URI);
+		mongoClient = new MongoClient(clientURI);
+		
+		MongoDatabase mongoDatabase = mongoClient.getDatabase("MyMongoDB");
+		collection = mongoDatabase.getCollection("weather");			
+	}
+
+	private static void closeMongoDB() {
+		mongoClient.close();			
+	}
+
+	private static void saveToMongoDB(String cityLocation, String resultJson) {
+		BasicDBObject weatherInfo = BasicDBObject.parse(resultJson);
+			
+		collection.deleteOne(Filters.eq("name",cityLocation));				
+		collection.insertOne(new Document(weatherInfo));	
+	}	
+	
+}
